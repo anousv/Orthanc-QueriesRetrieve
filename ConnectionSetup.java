f0740f631051dc5ab730d80a637901fc2cb5f1cd
anousv <anousonevongsalat@yahoo.com>
2017-07-16 21:10:57 +0200

    Authentication

--- ConnectionSetup.java
+++ ConnectionSetup.java
@@ -34,7 +34,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-import javax.swing.SwingConstants;
 
 import ij.plugin.PlugIn;
 
